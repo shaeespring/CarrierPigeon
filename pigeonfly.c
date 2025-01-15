d921949f31517bd9fd9e9196a47f86f3b74f41b0
shaeespring <shaeespring#@ gmail.com>
2025-01-15 10:24:09 -0500

    strcmp fix

--- pigeonfly.c
+++ pigeonfly.c
@@ -1,34 +1,30 @@
-/* 
+/*
 PIGEONFLY: Writes todo items to lists
 
 FUNCTIONALITY:
-"pigeonfly (listname) -m (message, enquoted)" to write to a file containing a to do list.
-Entering a valid message should prompt user to add an optional due date
+"pigeonfly (listname) -m (message, enquoted)" to write to a file containing a to
+do list. Entering a valid message should prompt user to add an optional due date
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-
-int main(int argc, char **argv)
-{
-    char *command = argv[2];
-    if (argc < 3)
-    {
-        puts("Invalid number of arguments.\nUsage: pigeonfly (listname) (item)");
-        return 0;
-    }
-
-    puts("Will execute sh with the following script :");
-    char *listname = argv[1];
-    puts(listname);
-
-    if (command == "-m")
-    {
-        char *message = argv[3];
-    }
-
-    puts("Starting now:");
-    //    system(PIGEONFLY);
+int main(int argc, char **argv) {
+  char *command = argv[2];
+  if (argc < 3) {
+    puts("Invalid number of arguments.\nUsage: pigeonfly (listname) (item)");
     return 0;
+  }
+
+  puts("Will execute sh with the following script :");
+  char *listname = argv[1];
+  puts(listname);
+
+  if (strcmp(command, "-m")) {
+    char *message = argv[3];
+  }
+
+  puts("Starting now:");
+  //    system(PIGEONFLY);
+  return 0;
 }
